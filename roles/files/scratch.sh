00b16a317e9843f81805a959de759140d1b3a26a
Mike Wilson <mike@soarcast.com>
2021-06-02 21:58:17 -0700

    darkice and icecast now working!

--- roles/files/scratch.sh
+++ roles/files/scratch.sh
@@ -1,5 +1,5 @@
 # This one starts it!
-./rx.py --nocrypt --args "rtl" --gains 'lna:49' -S 960000 -o 25000 -X -q 0 -v 1 -2 -V -U -T trunk.tsv -l 'http:0.0.0.0:8080'
+./rx.py --nocrypt --args "rtl" --gains 'lna:49' -S 960000 -o 25000 --audio-gain=3.5 -X -q 0 -v 1 -2 -V -U -T trunk.tsv -l 'http:0.0.0.0:8080' -O loop0
 
 
 # These ones are scratch from other startup scripts (including loopback support)
--- roles/files/startme-loop.sh
+++ roles/files/startme-loop.sh
@@ -1,1 +1,1 @@
-./rx.py --args "rtl" -N 'LNA:49' -n -S 960000 -O loop0 -n -f 772.84375e6 -o 25000 --audio-gain=3.5 -T trunk.tsv -V -2 -l 'http:0.0.0.0:8080' -U 2> stderr-stream0.2
+./rx.py --nocrypt --args "rtl" --gains 'lna:49' -S 960000 -o 25000 --audio-gain=3.5 -X -q 0 -v 1 -2 -V -U -T trunk.tsv -l 'http:0.0.0.0:8080' -O loop0 2> stderr-stream0.2
